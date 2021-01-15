ebd742278c6b224df0a7dfdc4ca626f3154e1bb9
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-01-15 14:27:15 +0600

    created a five let and console to them

---
+++ javacripts.js
@@ -0,0 +1,10 @@
+let first = "Shrek";
+let second = first;
+let third = second;
+let fourth = third;
+let fifth = fourth;
+
+let capital = "London";
+let country = "GB";
+console.log (fifth);
+console.log (capital +  " is a capital of " + country);
