4bc7c82f313a1cb7a3f9648d00643a588b20950e
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-01-21 20:42:50 +0600

    some examples from class

--- javacripts.js
+++ javacripts.js
@@ -1,42 +1,42 @@
-let first = "Shrek";
-let second = first;
-let third = second;
-let fourth = third;
-let fifth = fourth;
+// let first = "Shrek";
+// let second = first;
+// let third = second;
+// let fourth = third;
+// let fifth = fourth;
 
-let capital = "Bishkek";
-let country = "KG";
-console.log (fifth);
-console.log (capital +  " is a capital of " + country);
+// let capital = "Bishkek";
+// let country = "KG";
+// console.log (fifth);
+// console.log (capital +  " is a capital of " + country);
 
-let subject = "Distance";
-let unit = "meters";
+// let subject = "Distance";
+// let unit = "meters";
 
-subject = "Weight";
-unit = "gram";
+// subject = "Weight";
+// unit = "gram";
 
-subject = "Height";
-unit = "centimeter";
+// subject = "Height";
+// unit = "centimeter";
 
-console.log (subject + " is measured in " + unit);
+// console.log (subject + " is measured in " + unit);
 
-let title = "Dark Tower";
-let author = "SK";
+// let title = "Dark Tower";
+// let author = "SK";
 
-title = "Garry Potter";
-author = "JK Rowling";
-console.log ("My favourite book is " + title + " by " + author);
+// title = "Garry Potter";
+// author = "JK Rowling";
+// console.log ("My favourite book is " + title + " by " + author);
 
-console.log ('My favourite song is \"Yesterday\" by Beatles');
-console.log ('I\'m the best!');
+// console.log ('My favourite song is \"Yesterday\" by Beatles');
+// console.log ('I\'m the best!');
 
-let artist = "Beatles";
-// console.log ("I\'m the fan of ' +   artist  +  "!" );
-console.log (`I'm the fan of "`+ artist + `"!`);
+// let artist = "Beatles";
+// // console.log ("I\'m the fan of ' +   artist  +  "!" );
+// console.log (`I'm the fan of "`+ artist + `"!`);
 
-let movie = "Terminator";
-console.log (`My favourite movie is "` + movie +`" `);
-console.log (`My favourite movie is "${movie}"`);
+// let movie = "Terminator";
+// console.log (`My favourite movie is "` + movie +`" `);
+// console.log (`My favourite movie is "${movie}"`);
 
 
 // let firstName = prompt('What is your name?');
@@ -69,5 +69,81 @@ console.log (`My favourite movie is "${movie}"`);
 
 
 
+
+
+
+// let a = prompt("What is A?");
+// if (a) { 
+//  console.log ("A squared equals to " + (a ** 2));
+// }
+
+// else {
+//  console.log ("A is not set");
+// }
+
+// const KG_YEAR = 1991;
+// let year = prompt ("What is the day independence of Kyrgyzstan?");
+
+// if (year < KG_YEAR) {
+//     console.log ("too early");
+// }
+// else if (year > KG_YEAR) {
+//     console.log ("Too late");
+// }
+
+// else (year == KG_YEAR ) {
+//     console.log ("Exactly");
+// }
+
+
+
+
+
+// let a = +prompt ("What is A?");
+// let b = +prompt ("What is B?");
+// let c = a % b;
+
+
+// if (c){
+//     console.log ("A is divided by B with a remainder" + c);
+// }
+
+// else {
+//     console.log ("A is divided by B without a remainder");
+// }
+
+
+
+
+// let a = prompt ("What is A?");
+// let b = prompt ("What is B?");
+
+
+
+// if (a < b) {
+//  console.log ("A is smaller than B");
+//  }
+// else if (a > b) {
+// console.log ("A is bigger than B");
+//  }
+    
+// else (a == b ) {
+// console.log ("A is equal to B");
+// }
+
+
+
+
+// let a = prompt ("What is A?");
+// let b = prompt ("What is B?");
+// let c = a ** (1/2);
+
+// if (c == b) {
+//   console.log ("Yes!");
+// }
+
+// else {
+//   console.log ("No!");
+// }
 
 
